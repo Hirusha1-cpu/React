e37c709d7d4e28882c1406189d0fa71932430a30
Hirusha1-cpu <hirushafernando121@gmail.com>
2023-05-17 05:01:49 -0700

    MenuComponents

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React,{Component} from 'react';
+import {Navbar, NavbarBrand} from 'reactstrap'
 import './App.css';
+import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render(){
+    return (
+      <div>
+       <Navbar dark color="primary">
+          <div className='container'>
+            <NavbarBrand href='/'>
+              Hirusha Fernando
+
+            </NavbarBrand>
+          </div>
+       </Navbar>
+       <Menu dishes={this.state.dishes}/>
+      </div>
+    );
+  }
 }
-
 export default App;
